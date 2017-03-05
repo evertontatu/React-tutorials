b3c814d44d74d4a13c73228e8f8a9e1b28d8af4d
evertontatu <evertonferreira.de@gmail.com>
2017-03-04 21:23:23 -0300

    set Route with browserHistory,IndexRoute

--- src/Autor.js
+++ src/Autor.js
@@ -73,28 +73,28 @@ class TabelaAutores extends Component {
 
     render() {
         return(
-                    <div>
-                      <table className="pure-table">
-                        <thead>
-                          <tr>
-                            <th>Nome</th>
-                            <th>email</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                            this.props.lista.map(function(autor){
-                              return (
-                                <tr key={autor.id}>
-                                  <td>{autor.nome}</td>
-                                  <td>{autor.email}</td>
-                                </tr>
-                              );
-                            })
-                          }
-                        </tbody>
-                      </table>
-                    </div>
+          <div>
+            <table className="pure-table">
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.props.lista.map(function(autor){
+                    return (
+                      <tr key={autor.id}>
+                        <td>{autor.nome}</td>
+                        <td>{autor.email}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
         );
     }
 }
@@ -125,9 +125,13 @@ export default class AutorBox extends Component {
   render(){
     return (
       <div>
-        <FormularioAutor/>
-        <TabelaAutores lista={this.state.lista}/>
-
+        <div className="header">
+          <h1>Cadastro de autores</h1>
+        </div>
+        <div className="content" id="content">
+          <FormularioAutor/>
+          <TabelaAutores lista={this.state.lista}/>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorBox from './Autor';
+import Home from './Home';
 import './index.css';
-import {Router,Route} from 'react-router';
+import {Router,Route,browserHistory,IndexRoute} from 'react-router';
 
 ReactDOM.render(
-  (<Router>
-    <Route path="/" component={App}/>
-    <Route path="/autor"/>
-    <Route path="/livro"/>
+  (<Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/autor" component={AutorBox}/>
+      <Route path="/livro"/>
+    </Route>
   </Router>),
   document.getElementById('root')
 );
